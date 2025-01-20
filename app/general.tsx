5552747aaf7cc700e2ea6b6870cfe0bd2be012a8
tct123 <42028373+tct123@users.noreply.github.com>
2025-01-20 12:44:03 +0100

    .

--- app/general.tsx
+++ app/general.tsx
@@ -1,16 +1,35 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, FlatList } from 'react-native'
 import React from 'react'
+import background from "@/assets/images/background.jpg";
+import { useRouter } from 'expo-router';
+import CustomButton from '@/components/CustomButton'
 
 const General = () => {
+    const data = [{ id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' }, { id: '1', name: 'Home', path: '/' },]
+    const router = useRouter()
     return (
-        <View style={styles.container}>
-            <Text>general</Text>
-        </View>
+        <ImageBackground style={styles.bg} source={background}>
+            <View style={styles.headerview}>
+                <Text style={styles.title}>Choose example</Text>
+            </View>
+            <FlatList style={styles.flatlist} data={data} keyExtractor={(item) => item.id} renderItem={({ item }) => (
+                <View style={styles.container}>
+                    <CustomButton onPress={() => router.push(item.path)} title={item.name} />
+                </View>
+            )}>
+            </FlatList>
+        </ImageBackground>
     )
 }
 const styles = StyleSheet.create({
+    bg: { flex: 1, resizeMode: "cover" },
     container: {
-        flex: 1, justifyContent: 'center', alignItems: 'center'
-    }
+        flex: 1, justifyContent: "center", margin: 7,
+    },
+    flatlist: {
+        flex: 1,
+    },
+    headerview: { flex: 1, justifyContent: "center", alignItems: "center" },
+    title: { flex: 1, fontSize: 50, fontWeight: "bold" },
 })
 export default General
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import CustomButton from "@/components/CustomButton";
 import { Text, View, StyleSheet, ImageBackground } from "react-native";
 import { StatusBar } from "react-native";
-import background from "@/assets/images/background.jpg"
+import background from "@/assets/images/background.jpg";
 import { useRouter } from "expo-router";
 
 export default function Index() {
@@ -10,15 +10,21 @@ export default function Index() {
     <ImageBackground style={styles.bg}
       source={background} resizeMode="cover">
       <View
-        className="flex-1"
+        style={styles.topview}
       >
-        <CustomButton onPress={()=>router.push("/general")} title={"Test button"} />
         <StatusBar barStyle={"dark-content"} />
+        <Text style={styles.title}>My Portfolio</Text>
+      </View>
+      <View style={styles.bottomview}>
+        <CustomButton onPress={() => router.push("/general")} title={"Test button"} />
       </View>
     </ImageBackground>
   );
 }
 const styles = StyleSheet.create({
-  bg: { flex: 1, resizeMode: "cover", justifyContent: "center" }
+  bg: { flex: 1, resizeMode: "cover" },
+  topview: { flex: 1, justifyContent: "center", alignItems: "center" },
+  bottomview: { flex: 1, justifyContent: "center" },
+  title: { flex: 1, fontSize: 50, fontWeight: "bold" },
 }
 )
