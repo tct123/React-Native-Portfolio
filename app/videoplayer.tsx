c1f2928ce92d198e7871e812a211dc94e0008b1b
tct123 <42028373+tct123@users.noreply.github.com>
2025-01-20 23:54:08 +0100

    .

--- app/videoplayer.tsx
+++ app/videoplayer.tsx
@@ -1,17 +1,20 @@
 import { StyleSheet, View } from 'react-native'
 import React from 'react'
-import { Video } from 'expo-av'
+import { useVideoPlayer, VideoView } from 'expo-video'
 import { Asset } from 'expo-asset'
 
-const videoplayer = () => {
-    const video= Asset.fromModule(require("assets/video/video.mp4")).uri
+const Videoplayer = () => {
+    const video= Asset.fromModule(require("../assets/video/video.mp4")).uri
+    const player=useVideoPlayer(video,(player)=>{
+        player.play()
+    })
     return (
         <View>
-            <Video source={{ uri:video}} useNativeControls={true}></Video>
+            <VideoView player={player} />
         </View>
     )
 }
 
-export default videoplayer
+export default Videoplayer
 
 const styles = StyleSheet.create({})
