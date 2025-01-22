39bc11c51251a07ee4df0edc9ad061843f4b4f13
tct123 <42028373+tct123@users.noreply.github.com>
2025-01-22 14:49:44 +0100

    Update videoplayer.tsx

--- app/videoplayer.tsx
+++ app/videoplayer.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native'
+import { Dimensions, StyleSheet, View } from 'react-native'
 import React from 'react'
 import { useVideoPlayer, VideoView } from 'expo-video'
 import { Asset } from 'expo-asset'
@@ -6,15 +6,19 @@ import { Asset } from 'expo-asset'
 const Videoplayer = () => {
     const video = Asset.fromModule(require("../assets/video/video.mp4")).uri
     const player = useVideoPlayer(video, (player) => {
-        player.play()
+        player.play();
+        player.replay();
     })
     return (
         <View>
-            <VideoView player={player} />
+            <VideoView player={player} style={styles.video} allowsFullscreen allowsPictureInPicture />
         </View>
     )
 }
 
 export default Videoplayer
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({video:{
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+}})
