ed1a8423fcbbe698552c7a2f9c583c1e51e8a8d8
tct123 <42028373+tct123@users.noreply.github.com>
2025-01-22 16:16:14 +0100

    .

--- app/audioplayer.tsx
+++ app/audioplayer.tsx
@@ -1,13 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import {} from 'expo-audio'
+import { useAudioPlayer } from 'expo-audio'
+import { Asset } from 'expo-asset'
 
-const audioplayer = () => {
-  return (
-    <View>
-      <Text>audioplayer</Text>
-    </View>
-  )
+const Audioplayer = () => {
+    const MyAudio = Asset.fromModule(require("../assets/video/video.mp4")).uri;
+    const player = useAudioPlayer(MyAudio)
+    player.play()
+    return (
+        <View>
+            <Text>Audioplayer</Text>
+        </View>
+    )
 }
 
-export default audioplayer
+export default Audioplayer
