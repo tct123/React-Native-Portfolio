6399170ba3d9d91c75bda4e774b6af1fbf180b16
tct123 <42028373+tct123@users.noreply.github.com>
2025-01-22 16:20:24 +0100

    Update audioplayer.tsx

--- app/audioplayer.tsx
+++ app/audioplayer.tsx
@@ -4,7 +4,7 @@ import { useAudioPlayer } from 'expo-audio'
 import { Asset } from 'expo-asset'
 
 const Audioplayer = () => {
-    const MyAudio = Asset.fromModule(require("../assets/video/video.mp4")).uri;
+    const MyAudio = Asset.fromModule(require("../assets/audio/happy-birthday-whistled.wav")).uri;
     const player = useAudioPlayer(MyAudio)
     player.play()
     return (
