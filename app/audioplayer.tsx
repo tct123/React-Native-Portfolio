09e5526d0a544a903f59a30d9a5b311a2779dcd7
tct123 <42028373+tct123@users.noreply.github.com>
2025-01-22 15:55:42 +0100

    .

---
+++ app/audioplayer.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {} from 'expo-audio'
+
+const audioplayer = () => {
+  return (
+    <View>
+      <Text>audioplayer</Text>
+    </View>
+  )
+}
+
+export default audioplayer
