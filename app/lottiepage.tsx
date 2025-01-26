35773b00a491a0fc6c6681d476dde7939d4dc6c0
tct123 <42028373+tct123@users.noreply.github.com>
2025-01-26 02:04:38 +0100

    .

---
+++ app/lottiepage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const lottiepage = () => {
+  return (
+    <View>
+      <Text>lottiepage</Text>
+    </View>
+  )
+}
+
+export default lottiepage
